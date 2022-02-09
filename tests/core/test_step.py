7036155f177381c89b491fea1750e874109332ee
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-02-09 09:27:22 -0300

    added tests for completion

--- tests/core/test_step.py
+++ tests/core/test_step.py
@@ -1,5 +1,16 @@
 from apf.core.step import GenericStep
+from apf.producers import GenericProducer
 import pytest
+
+
+@pytest.fixture
+def basic_config():
+    return {
+        "CONSUMER_CONFIG": {
+            "PARAMS": {},
+            "CLASS": "apf.consumers.generic.GenericConsumer",
+        },
+    }
 
 
 def test_get_single_extra_metrics():
@@ -16,7 +27,10 @@ def test_get_single_extra_metrics():
     }
     message = {"oid": "TEST", "candid": 1}
     gs = GenericStep(config=config)
-    extra_metrics = gs.get_extra_metrics(message)
+    gs.message = message
+    gs._pre_execute()
+    gs._post_execute(None)
+    extra_metrics = gs.metrics
     assert type(extra_metrics) is dict
     assert type(extra_metrics["oid"]) is str
     assert type(extra_metrics["candid"]) is int
@@ -45,7 +59,10 @@ def test_get_batch_extra_metrics():
     }
     message = [{"oid": "TEST", "candid": 1}, {"oid": "TEST2"}, {"candid": 3}]
     gs = GenericStep(config=config)
-    extra_metrics = gs.get_extra_metrics(message)
+    gs.message = message
+    gs._pre_execute()
+    gs._post_execute(None)
+    extra_metrics = gs.metrics
     assert type(extra_metrics) is dict
     assert type(extra_metrics["oid"]) is list
     assert type(extra_metrics["candid"]) is list
@@ -99,3 +116,50 @@ def test_get_value():
 
     with pytest.raises(ValueError):
         gs.get_value(message, {"key": "oid", "alias": 1})
+
+
+def test_step_type(basic_config):
+    basic_config.update({"STEP_TYPE": "abcdefg"})
+    with pytest.raises(Exception):
+        GenericStep(basic_config)
+    basic_config.update({"STEP_TYPE": "composite"})
+    gs = GenericStep(basic_config)
+    assert gs.step_type == "composite"
+    basic_config.update({"STEP_TYPE": "component"})
+    gs = GenericStep(basic_config)
+    assert gs.step_type == "component"
+    basic_config.update({"STEP_TYPE": "simple"})
+    gs = GenericStep(basic_config)
+    assert gs.step_type == "simple"
+
+
+def test_without_consumer_config(basic_config):
+    basic_config.update({"CONSUMER_CONFIG": {}})
+    with pytest.raises(Exception):
+        GenericStep(basic_config)
+
+
+def test_with_producer_config(basic_config):
+    basic_config.update(
+        {"PRODUCER_CONFIG": {"CLASS": "apf.producers.generic.GenericProducer"}}
+    )
+    gs = GenericStep(basic_config)
+    assert isinstance(gs.producer, GenericProducer)
+
+
+def test_send_metrics(basic_config):
+    basic_config.update(
+        {
+            "METRICS_CONFIG": {
+                "CLASS": "apf.metrics.GenericMetricsProducer",
+                "PARAMS": {},
+            },
+        }
+    )
+    gs = GenericStep(basic_config)
+    gs.send_metrics(metric1="a", metric2="b")
+
+
+def test_start(basic_config):
+    gs = GenericStep(basic_config)
+    gs.start()
