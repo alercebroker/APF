7ba810317edac6886e8fabea00d9434ee53578ac
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-03-04 10:55:24 -0300

    update docstring

--- apf/core/templates/step/package/step.py
+++ apf/core/templates/step/package/step.py
@@ -7,11 +7,11 @@ class {{step_name}}(Step):
 
     Parameters
     ----------
-    consumer : GenericConsumer
-        Description of parameter `consumer`.
+    config : dict
+        Configuration for the step and its components
     **step_args : type
         Other args passed to step (DB connections, API requests, etc.)
 
     """
-    def __init__(self,config = None,level = logging.INFO,**step_args):
+    def __init__(self, config = None, level = logging.INFO, **step_args):
         super().__init__(config=config, level=level)
